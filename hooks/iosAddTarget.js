2f531c28a9d0fc183a57cf73449e13a95c694f12
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2019-03-28 17:30:56 +0900

    fix group identifier string

--- hooks/iosAddTarget.js
+++ hooks/iosAddTarget.js
@@ -153,7 +153,7 @@ function getPreferences(context, configXml, projectName) {
   var plist = projectPlistJson(context, projectName);
   var bundleid = getBundleId(context, configXml);
   var group = "group." + bundleid + BUNDLE_SUFFIX;
-  if (getCordovaParameter(configXml, 'GROUP_IDENTIFIER') !== "") {
+  if (getCordovaParameter(configXml, 'IOS_GROUP_IDENTIFIER') !== "") {
     group = getCordovaParameter(configXml, 'IOS_GROUP_IDENTIFIER');
   }
   return [{
